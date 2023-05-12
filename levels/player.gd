e627de23a4581529cf54f5df9e9b7989caa5bdc2
TheCrusty <zezoku@yahoo.com>
2023-05-11 21:24:23 -0400

    added enemies and made bullets that shoot

--- levels/player.gd
+++ levels/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
+var projectile = preload("res://levels/projectile.tscn")
 @export var speed = 400
 
 func get_input():
-	var input_direction = Input.get_vector("Rotate_Left", "Rotate_Right", "Forward", "Reverse")
-	velocity = input_direction * speed
+	if Input.is_action_pressed("Shoot"):
+		var projectile_instance = projectile.instantiate()
+		add_child(projectile_instance)
+	else:
+		var input_direction = Input.get_vector("Rotate_Left", "Rotate_Right", "Forward", "Reverse")
+		velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
-	look_at(get_global_mouse_position())
+	$Sprite2D.look_at(get_global_mouse_position())
 	move_and_slide()
