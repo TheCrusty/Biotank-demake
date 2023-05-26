42a16484e86148807fcececa2314f85dbcf82c2b
Brandon T. Wood <malceore@gmail.com>
2023-05-25 20:17:25 -0400

    Made movement actually tank like.

--- levels/player.gd
+++ levels/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-var projectile = preload("res://levels/projectile.tscn")
 @export var speed = 400
+@export var rotation_speed = 1.5
+
+var projectile = preload("res://levels/projectile.tscn")
+var rotation_direction = 0
+
 
 func get_input():
 	if Input.is_action_just_pressed("Shoot"):
@@ -9,10 +13,12 @@ func get_input():
 		owner.add_child(projectile_instance)
 		projectile_instance.transform = $TankTopPivot.global_transform
 	else:
-		var input_direction = Input.get_vector("Rotate_Left", "Rotate_Right", "Forward", "Reverse")
-		velocity = input_direction * speed
+		rotation_direction = Input.get_axis("Rotate_Left", "Rotate_Right")
+		velocity = transform.x * Input.get_axis("Forward", "Reverse") * speed
+
 
 func _physics_process(delta):
 	get_input()
+	rotation += rotation_direction * rotation_speed * delta	
 	$TankTopPivot.look_at(get_global_mouse_position())
 	move_and_slide()
