018e1ef9c01fb6a87ebad5fc605366a393d3a477
Brandon T. Wood <malceore@gmail.com>
2023-06-01 19:12:33 -0400

    made it so enemies spawn with one of random sprites.

--- levels/enemy.gd
+++ levels/enemy.gd
@@ -2,6 +2,10 @@ extends Sprite2D
 
 var health = 5
 
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	frame = rng.randi_range(0, 3)
+
 func takeDamage(amountDamage): 
 	health = health - amountDamage
 	if health <= 0:
