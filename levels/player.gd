2e7d4a1a46a11469d2f19cd29b2a49eb0b4a60d3
TheCrusty <zezoku@yahoo.com>
2023-06-15 21:25:36 -0400

    added acceleration and friction

--- levels/player.gd
+++ levels/player.gd
@@ -5,11 +5,17 @@ extends CharacterBody2D
 @export var damper = 0.015
 @export var rotation_speed = 3
 
+var engine_power = 800
+
+var acceleration = Vector2.ZERO
+
+var friction = -2
+
 var projectile = preload("res://levels/projectile.tscn")
 var rotation_direction = 0
 
 
-func get_input(delta):
+func get_input():
 	if Input.is_action_just_pressed("Shoot"):
 		var projectile_instance = projectile.instantiate()
 		projectile_instance.shooter = self
@@ -18,13 +24,22 @@ func get_input(delta):
 	else:
 		rotation_direction = Input.get_axis("Rotate_Left", "Rotate_Right")
 		if Input.is_action_pressed("Forward") or Input.is_action_pressed("Reverse"):
-			speed = lerp(speed,max_speed,damper)
-			velocity = transform.x * Input.get_axis("Forward", "Reverse") * (speed * delta) 
-			print(speed)
-		
+			acceleration = transform.x * engine_power * Input.get_axis("Forward", "Reverse")
 
 func _physics_process(delta):
-	get_input(delta)
-	rotation += rotation_direction * rotation_speed * delta	
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	velocity += acceleration * delta
+	rotation += rotation_direction * rotation_speed * delta
 	$TankTopPivot.look_at(get_global_mouse_position())
 	move_and_slide()
+	
+func apply_friction():
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	# friction increases with velocity, slows down more quickly the faster you are moving
+	var friction_force = velocity * friction
+	if velocity.length() < 100:
+		friction_force *= 3
+	acceleration += friction_force
