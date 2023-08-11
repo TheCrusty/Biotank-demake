1af9f4b36c25a3c2ede6af1f1f98edd2486f12be
Brandon T. Wood <malceore@gmail.com>
2023-08-10 20:57:59 -0400

    added third level.

--- gui/mission_select.gd
+++ gui/mission_select.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var first_mission = preload("res://levels/test_level.tscn").instantiate()
 var second_mission = preload("res://levels/level2.tscn").instantiate()
+var third_mission = preload("res://levels/level_3.tscn").instantiate()
 
 func _on_mission_one_pressed():
 	queue_free()
@@ -9,3 +10,6 @@ func _on_mission_one_pressed():
 func _on_mission_two_pressed():
 	queue_free()
 	get_tree().get_root().add_child(second_mission)
+func _on_mission_three_pressed():
+	queue_free()
+	get_tree().get_root().add_child(third_mission)
