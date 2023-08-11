18a61a00054b9f344aa7a46bf9b8d9e4d5ed8ce4
unknown <powpengo@gmail.com>
2023-08-10 20:47:18 -0400

    added level 2

--- gui/mission_select.gd
+++ gui/mission_select.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 var first_mission = preload("res://levels/test_level.tscn").instantiate()
-
+var second_mission = preload("res://levels/level2.tscn").instantiate()
 
 func _on_mission_one_pressed():
 	queue_free()
 	get_tree().get_root().add_child(first_mission)
+func _on_mission_two_pressed():
+	queue_free()
+	get_tree().get_root().add_child(second_mission)
